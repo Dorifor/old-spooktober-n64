4ea054dae1617f09355b300eb35494d2cdf90dfd
Bluestonered <63398176+Bluestonered@users.noreply.github.com>
2023-10-08 00:36:05 +0200

    Update player and icon

--- Player.gd
+++ Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 2.0
+const JUMP_VELOCITY = 1.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
--- README.md
+++
@@ -1,3 +0,0 @@
-# spooktober-n64
-An attempt to make a N64 inspired game for the Spooktober N64 Game Jam (2023)
-je suis un collabo
