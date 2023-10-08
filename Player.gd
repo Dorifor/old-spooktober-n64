006ffd66c618165a5c83dc67cd2b1a1a1036fa12
Mao <doryantilmant25@gmail.com>
2023-10-08 15:54:14 +0200

    player: first animations and script refactor

--- Player.gd
+++ Player.gd
@@ -4,25 +4,29 @@ extends CharacterBody3D
 var SPEED = 2.0
 const JUMP_VELOCITY = 1.5
 
-@export var sens_horizontal = 0.5
-@export var sens_vertical = 0.5
+@export var horizontal_sens = 0.5
+@export var vertical_sens = 0.5
 @export var walking_speed = 0.1
 @export var run_speed = 3
+
+@export var rig: Node3D
+@export var camera_mount: Node3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
-		$visuals.rotate_y(deg_to_rad(event.relative.x*sens_horizontal))
-		$Camera_mount.rotate_x((deg_to_rad(-event.relative.y * sens_vertical)))
+		rotate_y(deg_to_rad(-event.relative.x * horizontal_sens))
+		rig.rotate_y(deg_to_rad(event.relative.x * horizontal_sens))
+		camera_mount.rotate_x((deg_to_rad(-event.relative.y * vertical_sens)))
+	if event is InputEventKey and event.is_action("pause"):
+		get_tree().quit()
 
 func _physics_process(delta):
-	
 	if Input.is_action_pressed("run"):
 		SPEED = run_speed
 	else:
@@ -44,11 +48,9 @@ func _physics_process(delta):
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		
-		$visuals.look_at(position + direction)
+		rig.look_at(position + -direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	
-
