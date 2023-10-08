8ac1ca2153e761f3893fba055e933ded4b7a856f
Mao <doryantilmant25@gmail.com>
2023-10-08 23:30:41 +0200

    first lamp model + music player

--- assets/Globals.gd
+++ assets/Globals.gd
@@ -1,6 +1,6 @@
 extends Node
 
 @export var IS_GAME_PAUSED = false
-@export var VOLUME_VALUE = 0.7
+@export var VOLUME_VALUE = -34
 @export var HORIZONTAL_SENSIBILITY_VALUE = 0.2
 @export var VERTICAL_SENSIBILITY_VALUE = 0.1
