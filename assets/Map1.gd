1ea8728fd4a3e57ad9b74f4b59162c40ccd9128c
Mao <doryantilmant25@gmail.com>
2023-10-11 21:01:34 +0200

    fix: shallow players on client instances

--- assets/Map1.gd
+++ assets/Map1.gd
@@ -7,10 +7,12 @@ extends Node3D
 const Player = preload("res://assets/player/Player.tscn")
 
 func _ready():
+	await get_tree().process_frame
 	for player in Globals.PLAYER_DATA.keys():
 		add_player(player)
 		
 func add_player(peer_id):
+	if not is_multiplayer_authority(): return
 	var player = Player.instantiate()
 	player.name = str(peer_id)
 	add_child(player)
