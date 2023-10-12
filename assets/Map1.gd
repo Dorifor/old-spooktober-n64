b1a86192887e16fae9663619486517b5ee9366be
Bluestonered <63398176+Bluestonered@users.noreply.github.com>
2023-10-12 23:01:09 +0200

    ptite update

--- assets/Map1.gd
+++ assets/Map1.gd
@@ -5,20 +5,29 @@ extends Node3D
 @onready var root = $"."
 
 const Player = preload("res://assets/player/Player.tscn")
+const PropsPlayer = preload("res://assets/player/hider/hider.tscn")
 
 func _ready():
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
 	
 	await get_tree().process_frame
-	for player in Globals.PLAYER_DATA.keys():
-		add_player(player)
-		print(player)
+	#add_player(1)
+	add_PropsPlayer(1)
+#	for player in Globals.PLAYER_DATA.keys():
+#		add_player(player)
 		
 func add_player(peer_id):
 	if not is_multiplayer_authority(): return
 	var player = Player.instantiate()
 	player.name = str(peer_id)
 	add_child(player)
+	
+func add_PropsPlayer(peer_id):
+	if not is_multiplayer_authority(): return
+	var player = PropsPlayer.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
+	
 
 func _on_server_disconnected():
 	multiplayer.multiplayer_peer = null
